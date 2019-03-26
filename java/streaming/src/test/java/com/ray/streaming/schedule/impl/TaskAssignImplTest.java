2c2d9ab934dc215bc4d133eb8a3f33d36d3e6150
泰初 <zhenxuan.panzx@ant-financial.com>
2019-03-26 16:05:00 +0800

    use bazel and add unit test

---
+++ java/streaming/src/test/java/com/ray/streaming/schedule/impl/TaskAssignImplTest.java
@@ -0,0 +1,15 @@
+package com.ray.streaming.schedule.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+public class TaskAssignImplTest {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TaskAssignImplTest.class);
+
+  @Test
+  public void testTaskAssignImpl() {
+  }
+
+}
