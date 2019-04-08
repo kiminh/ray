eef809d1b357e6b52269531943a94302265a72af
lingxuan.zlx <skyzlxuan@gmail.com>
2019-04-08 15:57:13 +0800

    init streaming prototype in ray

---
+++ src/streaming/streaming_constant.h
@@ -0,0 +1,14 @@
+//
+// Created by ashione on 2019/4/1.
+//
+
+#ifndef STREAMING_PROTOTYPE_STREAMING_CONSTANT_H
+#define STREAMING_PROTOTYPE_STREAMING_CONSTANT_H
+#include <cstdlib>
+enum class StreamingStatus : uint32_t {
+  OK = 0,
+  MIN = OK,
+  MAX = OK
+};
+
+#endif //STREAMING_PROTOTYPE_STREAMING_CONSTANT_H
---
+++ src/streaming/streaming_consumer.h
@@ -0,0 +1,35 @@
+//
+// Created by ashione on 2019/4/1.
+//
+
+#ifndef STREAMING_PROTOTYPE_STREAMING_CONSUMER_H
+#define STREAMING_PROTOTYPE_STREAMING_CONSUMER_H
+#include "streaming_channel.h"
+#include "streaming_transfer.h"
+
+class StreamingConsumer : public StreamingChannel{
+ public:
+  StreamingConsumer(std::shared_ptr<StreamingChannelConfig> channel_config,
+                    std::shared_ptr<StreamingTransfer> transfer)
+    : StreamingChannel(channel_config, transfer) {}
+
+  StreamingStatus InitChannel() override {
+    transfer_->InitTransfer(channel_config_.operator*(), StreamingTransferRole::StreamingConsumer);
+    return StreamingStatus::OK;
+  }
+
+  StreamingStatus DestoryChannel() override{
+    transfer_->DestoryTransfer();
+    return StreamingStatus::OK;
+  }
+
+  StreamingStatus ConsumeMessage(std::shared_ptr<StreamingMessage> &msg) {
+    // return transfer_->ConsumeMessage(msg);
+    StreamingChannelInfo fake_info;
+    return strategy_implementor_->ConsumeMessage(fake_info,
+      std::bind(&StreamingTransfer::ConsumeMessage, transfer_, std::ref(fake_info), std::ref(msg)));
+    // Update channel info by fake info
+  }
+};
+
+#endif //STREAMING_PROTOTYPE_STREAMING_CONSUMER_H
