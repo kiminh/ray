8ee052d34843d28b831fc6a7ba616d3b3792e75b
senlin.zsl <senlin.zsl@antfin.com>
2019-08-14 10:59:38 +0800

    import rapidjson

---
+++ src/ray/util/json.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <string>
+#include <stdexcept>
+
+#ifndef RAPIDJSON_HAS_STDSTRING
+  #define RAPIDJSON_HAS_STDSTRING 1
+#endif
+
+#ifndef RAPIDJSON_THROWPARSEEXCEPTION
+  #define RAPIDJSON_THROWPARSEEXCEPTION 1
+#endif
+
+// Use exception for catching assert
+#ifdef _MSC_VER
+#pragma warning(disable : 4127)
+#endif
+
+#ifdef __clang__
+#pragma GCC diagnostic push
+#if __has_warning("-Wdeprecated")
+#pragma GCC diagnostic ignored "-Wdeprecated"
+#endif
+#endif
+
+#ifdef __clang__
+#pragma GCC diagnostic pop
+#endif
+
+// Not using noexcept for testing RAPIDJSON_ASSERT()
+#define RAPIDJSON_HAS_CXX11_NOEXCEPT 0
+
+#ifndef RAPIDJSON_ASSERT
+#define RAPIDJSON_ASSERT(x) (!(x) ? throw std::logic_error(RAPIDJSON_STRINGIFY(x)) : (void)0u)
+#endif
+
+// Make parse exception clear
+#define RAPIDJSON_PARSE_ERROR_NORETURN(parseErrorCode,offset) \
+  throw std::runtime_error(std::string("JSON parse error: ") + \
+    GetParseError_En(parseErrorCode) + \
+    " (" + std::to_string(offset) + ")")
+
+#include "src/ray/thirdparty/rapidjson/error/en.h"
+#include "src/ray/thirdparty/rapidjson/document.h"
+#include "src/ray/thirdparty/rapidjson/reader.h"
+#include "src/ray/thirdparty/rapidjson/writer.h"
+#include "src/ray/thirdparty/rapidjson/stringbuffer.h"
+
+namespace rapidjson {
+inline std::string to_string(const rapidjson::Document &doc) {
+  rapidjson::StringBuffer buffer;
+  rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+  doc.Accept(writer);
+  return buffer.GetString();
+}
+}
