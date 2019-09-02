244d65c36215a6b2b5389fbdaaefec0a1a9b83c4
刘宝 <po.lb@antfin.com>
2019-09-02 15:18:29 +0800

    Add HttpAsyncClient

--- src/ray/http/http_client.h
+++ src/ray/http/http_client.h
@@ -2,9 +2,14 @@
 
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
+#include <boost/bind.hpp>
+#include "ray/util/logging.h"
+
+#include <list>
 
 using tcp = boost::asio::ip::tcp;
-namespace http = boost::beast::http;
+namespace beast = boost::beast;
+namespace http = beast::http;
 
 namespace ray {
 class HttpSyncClient {
@@ -67,7 +72,7 @@ class HttpSyncClient {
     http::write(socket_, req);
 
     // This buffer is used for reading and must be persisted
-    boost::beast::flat_buffer buffer;
+    beast::flat_buffer buffer;
 
     // Declare a container to hold the response
     http::response<http::string_body> res;
@@ -109,7 +114,7 @@ class HttpSyncClient {
     http::write(socket_, req);
 
     // This buffer is used for reading and must be persisted
-    boost::beast::flat_buffer buffer;
+    beast::flat_buffer buffer;
 
     // Declare a container to hold the response
     http::response<http::string_body> res;
@@ -125,5 +130,189 @@ class HttpSyncClient {
   tcp::socket socket_;
   std::string host_;
   bool is_connected_ = false;
+};
+
+class HttpAsyncClient : public std::enable_shared_from_this<HttpAsyncClient> {
+  typedef std::function<void(boost::system::error_code ec, const std::string &&)>
+      Callback;
+
+ public:
+  HttpAsyncClient(boost::asio::io_context &ioc) : ioc_(ioc), socket_(ioc) {}
+
+  ~HttpAsyncClient() { Close(); }
+
+  bool Connect(const std::string &host, int port) {
+    if (is_connected_) {
+      return true;
+    }
+
+    host_ = host;
+    tcp::resolver resolver{ioc_};
+    auto results = resolver.resolve(host, std::to_string(port));
+    boost::asio::connect(socket_, results.begin(), results.end());
+    is_connected_ = true;
+    is_working_ = false;
+    return true;
+  }
+
+  void Close() {
+    if (!is_connected_) {
+      return;
+    }
+
+    // Gracefully close the socket
+    boost::system::error_code ec;
+    socket_.shutdown(tcp::socket::shutdown_both, ec);
+    if (ec && ec != boost::system::errc::not_connected) {
+      // TODO(hc):
+    }
+    is_connected_ = false;
+    is_working_ = false;
+
+    // Notify all callbacks in queue
+    const boost::system::error_code cancel_ec =
+        boost::system::errc::make_error_code(boost::system::errc::operation_canceled);
+    for (auto &i : queue_) {
+      i.second(cancel_ec, std::string());
+    }
+    queue_.clear();
+  }
+
+  void Get(const std::string &uri,
+           const std::unordered_map<std::string, std::string> &params,
+           Callback &&callback) {
+    if (!is_connected_) {
+      callback(boost::system::errc::make_error_code(boost::system::errc::not_connected),
+               std::string());
+      return;
+    }
+
+    std::ostringstream ostr;
+    ostr << uri;
+    auto itr = params.begin();
+    if (itr != params.end()) {
+      ostr << "?";
+    }
+    for (; itr != params.end();) {
+      ostr << itr->first;
+      if (!itr->second.empty()) {
+        ostr << "=" << itr->second;
+      }
+      if (++itr != params.end()) {
+        ostr << "&";
+      }
+    }
+    std::string target = ostr.str();
+
+    auto req = std::make_shared<http::request<http::string_body>>();
+    req->version(11);
+    req->method(http::verb::get);
+    req->target(target);
+    req->set(http::field::host, host_);
+    req->set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+    queue_.emplace_back(std::make_pair(req, callback));
+    exec_one();
+  }
+
+  void Post(const std::string &uri,
+            const std::unordered_map<std::string, std::string> &params,
+            std::string &&data, Callback &&callback) {
+    if (!is_connected_) {
+      callback(boost::system::errc::make_error_code(boost::system::errc::not_connected),
+               std::string());
+      return;
+    }
+
+    std::ostringstream ostr;
+    ostr << uri;
+    auto itr = params.begin();
+    if (itr != params.end()) {
+      ostr << "?";
+    }
+    for (; itr != params.end();) {
+      ostr << itr->first;
+      if (!itr->second.empty()) {
+        ostr << "=" << itr->second;
+      }
+      if (++itr != params.end()) {
+        ostr << "&";
+      }
+    }
+    std::string target = ostr.str();
+
+    auto req = std::make_shared<http::request<http::string_body>>();
+    req->version(11);
+    req->method(http::verb::post);
+    req->target(target);
+    req->set(http::field::host, host_);
+    req->set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+    req->set(http::field::content_length, data.size());
+    req->body() = std::move(data);
+    req->prepare_payload();
+
+    queue_.emplace_back(std::make_pair(req, callback));
+    exec_one();
+  }
+
+ private:
+  void on_write(boost::system::error_code ec, std::size_t bytes_transferred,
+                std::shared_ptr<http::request<http::string_body>> req,
+                Callback callback) {
+    boost::ignore_unused(bytes_transferred);
+    boost::ignore_unused(req);
+    if (ec) {
+      RAY_LOG(ERROR) << "on_write " << host_ << " failed, err: " << ec.message();
+      Close();
+      return;
+    }
+
+    auto res = std::make_shared<http::response<http::string_body>>();
+    auto buffer = std::make_shared<beast::flat_buffer>();
+
+    http::async_read(
+        socket_, *buffer, *res,
+        boost::bind(&HttpAsyncClient::on_read, shared_from_this(),
+                    boost::asio::placeholders::error,
+                    boost::asio::placeholders::bytes_transferred, res, buffer, callback));
+  }
+
+  void on_read(boost::system::error_code ec, std::size_t bytes_transferred,
+               std::shared_ptr<http::response<http::string_body>> res,
+               std::shared_ptr<beast::flat_buffer> buffer, Callback callback) {
+    boost::ignore_unused(bytes_transferred);
+    boost::ignore_unused(buffer);
+    if (ec) {
+      RAY_LOG(ERROR) << "on_read " << host_ << " failed, err: " << ec.message();
+      Close();
+      return;
+    }
+
+    callback(ec, std::move(res->body()));
+    is_working_ = false;
+    exec_one();
+  }
+
+  void exec_one() {
+    if (is_connected_ && !is_working_ && !queue_.empty()) {
+      is_working_ = true;
+      auto e = queue_.front();
+      queue_.pop_front();
+      http::async_write(
+          socket_, *e.first,
+          boost::bind(&HttpAsyncClient::on_write, shared_from_this(),
+                      boost::asio::placeholders::error,
+                      boost::asio::placeholders::bytes_transferred, e.first, e.second));
+    }
+  }
+
+ private:
+  boost::asio::io_context &ioc_;
+  std::string host_;
+  tcp::socket socket_;
+  bool is_connected_ = false;
+  bool is_working_ = false;
+  std::list<std::pair<std::shared_ptr<http::request<http::string_body>>, Callback>>
+      queue_;
 };
 }  // namespace ray
--- src/ray/http/http_test.cc
+++ src/ray/http/http_test.cc
@@ -8,6 +8,7 @@
 
 #include <memory>
 #include <thread>
+#include <vector>
 
 using tcp = boost::asio::ip::tcp;
 
@@ -21,7 +22,7 @@ class HttpTest : public ::testing::Test {
       std::make_shared<HttpServer>(ioc)->Start(host, port);
       ioc.run();
     });
-    (void)t;
+    boost::ignore_unused(t);
   }
 
  protected:
@@ -32,20 +33,21 @@ class HttpTest : public ::testing::Test {
 TEST_F(HttpTest, ServerParseParams) {
   {
     StartServer();
-    HttpRouter::Register("/test/parse/params",
-                         "get with any key value, e.g. http://127.0.0.1/test/parse/params?k1=v1&k2=v2",
-                         [](HttpParams &&params, const std::string &data, HttpReply &r) {
-                           rapidjson::Document doc(rapidjson::kObjectType);
-                           for (auto &param : params) {
-                             rapidjson::Value key;
-                             key.Set(param.first, doc.GetAllocator());
+    HttpRouter::Register(
+        "/test/parse/params",
+        "get with any key value, e.g. http://127.0.0.1/test/parse/params?k1=v1&k2=v2",
+        [](HttpParams &&params, const std::string &data, HttpReply &r) {
+          rapidjson::Document doc(rapidjson::kObjectType);
+          for (auto &param : params) {
+            rapidjson::Value key;
+            key.Set(param.first, doc.GetAllocator());
 
-                             rapidjson::Value val;
-                             val.Set(param.second, doc.GetAllocator());
-                             doc.AddMember(key, val, doc.GetAllocator());
-                           }
-                           r.SetJsonContent(rapidjson::to_string(doc));
-                         });
+            rapidjson::Value val;
+            val.Set(param.second, doc.GetAllocator());
+            doc.AddMember(key, val, doc.GetAllocator());
+          }
+          r.SetJsonContent(rapidjson::to_string(doc));
+        });
   }
 
   {
@@ -64,13 +66,46 @@ TEST_F(HttpTest, ServerParseParams) {
       ASSERT_EQ(expected[item.name.GetString()], item.value.GetString());
     }
   }
+
+  {
+    boost::asio::io_context ioc;
+    auto client = std::make_shared<HttpAsyncClient>(ioc);
+    client->Connect(host, port);
+
+    std::unordered_map<std::string, std::string> expected{{"k1", "v1"}, {"k2", "v2"}};
+    std::vector<bool> finish(10, false);
+
+    for (auto i = 0; i < finish.size(); ++i) {
+      client->Get(
+          "/test/parse/params", expected,
+          [&expected, &finish, i](boost::system::error_code ec, const std::string &s) {
+            if (ec) {
+              throw ec.message();
+            }
+            rapidjson::Document doc;
+            doc.Parse(s);
+            auto obj = doc.GetObject();
+
+            ASSERT_EQ(expected.size(), obj.MemberCount());
+            for (auto &item : obj) {
+              ASSERT_TRUE(expected.count(item.name.GetString()));
+              ASSERT_EQ(expected[item.name.GetString()], item.value.GetString());
+            }
+            finish[i] = true;
+          });
+    }
+
+    while (true) {
+      ioc.run_one();
+      if (std::all_of(finish.begin(), finish.end(), [](bool v) { return v; })) break;
+    }
+  }
 }
 
 TEST_F(HttpTest, ServerParseData) {
   {
     StartServer();
-    HttpRouter::Register("/test/parse/data",
-                         "post any json string",
+    HttpRouter::Register("/test/parse/data", "post any json string",
                          [](HttpParams &&params, const std::string &data, HttpReply &r) {
                            rapidjson::Document doc;
                            doc.Parse(data);
@@ -91,6 +126,35 @@ TEST_F(HttpTest, ServerParseData) {
     ASSERT_TRUE(doc.IsObject());
     ASSERT_EQ("ray", std::string(doc["name"].GetString()));
     ASSERT_EQ("post", std::string(doc["test"].GetString()));
+  }
+
+  {
+    boost::asio::io_context ioc;
+    auto client = std::make_shared<HttpAsyncClient>(ioc);
+    client->Connect(host, port);
+
+    std::vector<bool> finish(10, false);
+
+    for (auto i = 0; i < finish.size(); ++i) {
+      client->Post("/test/parse/data", {}, R"({"name":"ray","test":"post"})",
+                   [&finish, i](boost::system::error_code ec, const std::string &s) {
+                     if (ec) {
+                       throw ec.message();
+                     }
+                     rapidjson::Document doc;
+                     doc.Parse(s);
+
+                     ASSERT_TRUE(doc.IsObject());
+                     ASSERT_EQ("ray", std::string(doc["name"].GetString()));
+                     ASSERT_EQ("post", std::string(doc["test"].GetString()));
+                     finish[i] = true;
+                   });
+    }
+
+    while (true) {
+      ioc.run_one();
+      if (std::all_of(finish.begin(), finish.end(), [](bool v) { return v; })) break;
+    }
   }
 }
 }  // namespace ray
