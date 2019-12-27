39d492d9267007df82f32d2fcc9e4704e1c9f4f0
chentianyi <chentianyi.cty@antfin.com>
2019-12-27 16:44:09 +0800

    Remove env judgement.

--- streaming/java/streaming-runtime/src/main/java/org/ray/streaming/runtime/config/Config.java
+++ streaming/java/streaming-runtime/src/main/java/org/ray/streaming/runtime/config/Config.java
@@ -4,15 +4,9 @@ import java.io.Serializable;
 
 import javax.accessibility.Accessible;
 
-import org.ray.streaming.runtime.config.converter.EnvConverter;
-
 /**
  * Basic config interface.
  */
 public interface Config extends org.aeonbits.owner.Config, Accessible, Serializable {
 
-    @DefaultValue("dev")
-    @ConverterClass(EnvConverter.class)
-    @Key(value = "streaming.env")
-    String env();
 }
--- streaming/java/streaming-runtime/src/main/java/org/ray/streaming/runtime/util/EnvUtil.java
+++ streaming/java/streaming-runtime/src/main/java/org/ray/streaming/runtime/util/EnvUtil.java
@@ -4,17 +4,7 @@ import java.lang.management.ManagementFactory;
 
 import org.ray.runtime.RayNativeRuntime;
 
-/**
- *
- */
 public class EnvUtil {
-
-  private static final String ENV_KEY = "RAY_STREAMING_ENV";
-
-  public static boolean isOnlineEnv() {
-    String env = System.getenv(ENV_KEY);
-    return env != null;
-  }
 
   public static String getJvmPid() {
     return ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
